8a6e2dc8c2d89268073b895c0af4a251c015caab
pedro-dominici <pedro_dominici@outlook.com>
2022-07-12 13:08:10 -0300

    feat: chat

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello world!!!");
+/* eslint-disable n/no-path-concat */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/pages/index.html");
+});
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + "/pages/chat.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server on - PORT: 3001 ✅");
+});
